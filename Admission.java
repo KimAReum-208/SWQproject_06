1251e8d49d40a4973904a8abccf94d21843f43bc
KimAreum <rladkfma2030@naver.com>
2020-05-29 11:17:22 +0900

    주석문 기입 및 나이를 입력받는 메소드를 작성하였습니다.

---
+++ Admission.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * ABC수족관 입장료 처리 프로그램
+ * 
+ * @author (2017315002 김아름, 2017315038 이수정, 2018315051 키타야마 요시카) 
+ * @version (2019.05.29)
+ */
+public class Admission
+{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        //나이를 입력받아 age에 저장한다
+        System.out.print("나이를 입력해 주세요.");
+        int age = scanner.nextInt();
+        scanner.close();
+    }
+    public void AdmissionCalculator(int age){
+    }
+}
